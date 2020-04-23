aaba26b099b0d6c91699301a2b9bba2a7668668a
Yaron <yasegev@cisco.com>
2020-04-23 16:49:14 +0300

    initial commit

---
+++ src/main/java/com/example/demo/BatchConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.ApplicationContextFactory;
+import org.springframework.batch.core.configuration.support.GenericApplicationContextFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing(modular = true)
+public class BatchConfig {
+
+    @Bean
+    public ApplicationContextFactory getDummyJob() {
+        return new GenericApplicationContextFactory(Dummy1.class);
+    }
+
+    @Bean
+    public ApplicationContextFactory getJobConfig() {
+        return new GenericApplicationContextFactory(Dummy2.class);
+    }
+}
---
+++ src/main/java/com/example/demo/Dummy1.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Dummy1 {
+
+    @Autowired
+    JobBuilderFactory jobs;
+    @Autowired
+    StepBuilderFactory steps;
+
+    @Bean
+    public Step step1() {
+        return steps.get("dummy-tasklet-1")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    System.out.println("hello 1");
+                    return RepeatStatus.FINISHED;
+                })).build();
+    }
+
+    @Bean
+    public Job job() {
+        return jobs.get("dummy-job-1")
+                .start(step1())
+                .build();
+    }
+}
